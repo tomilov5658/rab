1ce3c28fe598c5fd5f839c8227f92902572f27cd
Андрей яковлев <ssilver706@gmail.com>
2021-12-22 00:45:54 +0700

    umnoz

--- lb4/ConsoleApp1/Program.cs
+++ lb4/ConsoleApp1/Program.cs
@@ -12,6 +12,26 @@ namespace ConsoleApp1
             int B = int.Parse(Console.ReadLine());
             Console.WriteLine("Выберите действие: \n1) +\n2) -\n3) *\n4) /");
             int vari = int.Parse(Console.ReadLine());
+
+            void pluss(int a, int b)
+            {
+                Console.WriteLine(a + b);
+            }
+            void minus(int a, int b)
+            {
+                Console.WriteLine(a - b);
+            }
+            void devide(int a, int b)
+            {
+                double _1 = (double)a;
+                double _2 = (double)b;
+                Console.WriteLine(_1 / _2);
+            }
+            void multy(int a, int b)
+            {
+                Console.WriteLine(a * b);
+
+            }
         }
     }
 }
