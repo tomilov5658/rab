33f0c74643d1217eed10be2f983d6f2387d75273
Андрей яковлев <ssilver706@gmail.com>
2021-12-22 00:36:48 +0700

    plus

--- lb4/ConsoleApp1/Program.cs
+++ lb4/ConsoleApp1/Program.cs
@@ -6,7 +6,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите первое число");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            int B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Выберите действие: \n1) +\n2) -\n3) *\n4) /");
+            int vari = int.Parse(Console.ReadLine());
+
+            void pluss(int a, int b)
+            {
+                Console.WriteLine(a + b);
+            }
         }
     }
 }
